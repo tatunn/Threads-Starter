e1f41c5216e3a26f58dcb2ffb5ef2eda84388eda
nikoloz tatunashvili <nikoloz.tatunashvili@gmail.com>
2020-07-21 17:31:01 +0400

    Initial Commit

---
+++ LB Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LB Task
+//
+//  Created by Nikoloz Tatunashvili on 7/21/20.
+//  Copyright © 2020 Liberty Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
